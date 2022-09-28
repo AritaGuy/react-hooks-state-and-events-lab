566766fb39c632fe0971ee40cbc6962b191bddeb
Sam Arita <arita.sam@student.moringaschool.com>
2022-09-28 03:58:33 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import items from "../data/items";
 
 function App() {
+ 
+
+  const[mode, setMode] = useState(
+    false
+  )
+  
+  
+
+  function toogle2(){
+   setMode(!mode)
+  }
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  const appClass = mode ? "App dark" : "App light"
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toogle2} >Dark Mode</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
